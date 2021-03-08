49f62217b0a593d21923ab92fb19db9c16a2cdea
Janis Krasemann <janis.krasemann@enpit.de>
2021-03-08 11:33:10 +0100

    Add skills section

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,38 @@
+import React from 'react';
 import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
+import TextTransition, { presets } from "react-text-transition";
 
+import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import SocialMedia from '../components/social-media/social-media';
 
+const SKILLS = [
+  'Typescript',
+  'HTML',
+  'CSS',
+  'Java',
+  'React',
+  'JavaScript',
+  'Angular',
+  'Spring',
+  'Scrum',
+  'AWS'
+];
+
+function getRandomSkill(skill) {
+  const randomSkill = Math.floor(Math.random() * SKILLS.length)
+  return randomSkill !== skill ? randomSkill : getRandomSkill(skill);
+}
+
 export default function Home() {
+  const [skill, setSkill] = React.useState(0);
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setSkill(getRandomSkill(skill));
+    }, 3000);
+    return () => clearTimeout(interval);
+  });
+
   return (
     <Layout home>
       <Head>
@@ -12,11 +40,20 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>
-          JavaScript enthusiast (mainly React, but also Angular of late).
-          Developing software for <a href="https://www.enpit.de" target="_blank">@enpit</a>.
+          I develop software for <a href="https://www.enpit.de" target="_blank">@enpit</a>
+          <br />
+          using
+          {" "}
+          <TextTransition
+            text={ SKILLS[skill] }
+            springConfig={presets.stiff}
+            inline={true}
+          />
+          {" "}
+          and a lot of passion.
         </p>
-        <SocialMedia twitter github instagram/>
       </section>
+      <SocialMedia twitter github instagram/>
     </Layout>
   );
 }
